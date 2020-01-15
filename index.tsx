945193de20586f4fae5a9b89cc679a369d36b315
Kvn91 <k.dargere@gmail.com>
2020-01-15 17:29:27 +0100

    1 file modified

--- index.tsx
+++ index.tsx
@@ -7,14 +7,3 @@ import './style.css';
 import App from './App';
 import reducers from './reducers';
 import { getRestaurants } from './actions/restaurants.actions';
-
-const store = createStore(reducers, applyMiddleware(thunk));
-
-store.dispatch(getRestaurants());
-
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
